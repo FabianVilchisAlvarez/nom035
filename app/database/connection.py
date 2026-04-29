11461b9ec8385ed559f5d16e9c54f28c284bc4d0
Fabian Vilchis <vilchis.fabian@gmail.com>
2026-04-29 13:32:31 -0600

    fix: connection a postgres con env y ssl para render

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:admin@localhost/nom035"
+# 🔥 Tomar la variable de entorno (Render)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# 🔒 Validación básica (opcional pero útil)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está configurada")
+
+# 🔥 FIX RENDER (PostgreSQL requiere SSL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
