dc4795a707080a7596b12991ec7aa84fdc1e90ac
Fabian Vilchis <vilchis.fabian@gmail.com>
2026-04-30 10:06:21 -0600

    fix cors production

--- app/main.py
+++ app/main.py
@@ -47,9 +47,13 @@ if settings.ENV == "development":
 # =========================
 # 🌐 CORS
 # =========================
+origins = [
+    "https://nom035-frontend.onrender.com"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
